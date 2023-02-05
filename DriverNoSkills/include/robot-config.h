a3afe7df736f3350895efa7dee9f091b72e526a7
Archmedia <81200440+VscoKiwi@users.noreply.github.com>
2023-02-04 19:42:28 -0800

    Create robot-config.h

---
+++ DriverNoSkills/include/robot-config.h
@@ -0,0 +1,19 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor left_front;
+extern motor right_front;
+extern motor left_back;
+extern motor right_back;
+extern motor Roller;
+extern motor Cata;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
